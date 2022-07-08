13d999a4445ca8bb2f895a93586a6ae8b924b52a
Alon Albert <aalbert@google.com>
2022-07-08 08:07:49 -0700

    Pretty print

--- src/main/kotlin/com/aa/plexautodelete/Main.kt
+++ src/main/kotlin/com/aa/plexautodelete/Main.kt
@@ -13,23 +13,28 @@ import kotlinx.cli.default
 import java.io.File
 import java.time.Duration
 import java.time.Instant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.logging.Level
+import java.util.logging.Level.ALL
+import java.util.logging.Level.CONFIG
 import java.util.logging.Level.FINE
+import java.util.logging.Level.FINER
+import java.util.logging.Level.FINEST
 import java.util.logging.Level.INFO
+import java.util.logging.Level.OFF
 import java.util.logging.Level.SEVERE
+import java.util.logging.Level.WARNING
+import java.util.logging.Level.parse
 import kotlin.system.exitProcess
 
-private val DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").withZone(ZoneId.systemDefault())
 val DEFAULT_CONFIG_FILE = "${System.getProperty("user.home")}/.plex-auto-delete-config.json"
+
+private val logLevels = listOf(OFF, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST, ALL)
 
 fun main(vararg args: String) {
   val parser = ArgParser("Plex Auto Delete")
   val configFile by parser.option(ArgType.String, shortName = "c", description = "Config file").default(DEFAULT_CONFIG_FILE)
-  val logLevel by parser.option(ArgType.Choice<Level>(listOf(INFO), { Level.parse(it) }, { it.name }), shortName = "v", description = "Log level")
-    .default(FINE)
+  val logLevel by parser.option(ArgType.Choice<Level>(logLevels, { parse(it) }, { it.name }), shortName = "v", description = "Log level").default(FINER)
   val logFile by parser.option(ArgType.String, shortName = "l", description = "Log file")
   val interactive by parser.option(ArgType.Boolean, shortName = "i", description = "Interactive mode").default(false)
   parser.parse(arrayOf(*args))
@@ -48,23 +53,28 @@ fun main(vararg args: String) {
     val watchedEpisodes = server.getWatchedEpisodes(config.tvSections, config.users.first(), config.days)
     val maxNameLen = watchedEpisodes.maxOf { it.name.length }
     val maxShowNameLen = watchedEpisodes.maxOf { it.showName.length }
-    logger.fine("Deletion candidates:")
-    val episodesToDelete = watchedEpisodes.mapNotNull { episode ->
-      logger.fine("  ${episode.toDisplayString(maxNameLen, maxShowNameLen, now)}")
 
-      val unwatchedBy = config.users.filter { it.shows.isIncluded(episode.showName) }.map { user ->
-        WatchedBy(user.name, server.isWatchedBy(episode.key, user.plexToken))
-      }.filter { !it.watched }.map { it.user }
-      if (unwatchedBy.isNotEmpty()) {
-        logger.fine("    Unwatched by ${unwatchedBy.joinToString()}")
-        null
-      } else {
-        episode
+    logger.fine("Deletion candidates:")
+    val episodesToDelete = watchedEpisodes.groupBy { it.showName }.flatMap { (showName, episodes) ->
+      logger.fine("    $showName:")
+      episodes.mapNotNull { episode ->
+        val unwatchedByUsers = config.users.filter { it.shows.isIncluded(episode.showName) }.map { user ->
+          WatchedBy(user.name, server.isWatchedBy(episode.key, user.plexToken))
+        }.filter { !it.watched }.map { it.user }
+
+        val episodeString = "${episode.toEpisodeId()}: ${episode.name.padEnd(maxNameLen)}"
+        if (unwatchedByUsers.isEmpty()) {
+          logger.fine("        $episodeString Can be deleted")
+          episode
+        } else {
+          logger.fine("        $episodeString Unwatched by ${unwatchedByUsers.joinToString()}")
+          null
+        }
       }
     }
 
     if (episodesToDelete.isEmpty()) {
-      logger.fine("Nothing to delete")
+      logger.info("Nothing to delete")
     } else {
       if (interactive) {
         println("Marked for deletion:")
@@ -77,12 +87,12 @@ fun main(vararg args: String) {
           exitProcess(0)
         }
       }
-      logger.fine("Deleting episodes:")
+      logger.info("Deleting episodes:")
       var totalFiles = 0
       var totalSize = 0L
       val plexToken = config.users.first().plexToken
       episodesToDelete.forEach { episode ->
-        logger.fine("  ${episode.toDisplayString(maxNameLen, maxShowNameLen, now)}")
+        logger.info("  ${episode.toDisplayString(maxNameLen, maxShowNameLen, now)}")
         server.getFiles(episode.key, plexToken).forEach files@{ file ->
           if (!file.exists()) {
             logger.warning("File ${file.path} not found. Is the section directory mounted?")
@@ -102,7 +112,7 @@ fun main(vararg args: String) {
       val (appToken, userToken) = config.pushoverConfig
       Pushover.send(appToken, userToken, message)
 
-      logger.fine(message)
+      logger.info(message)
     }
   } catch (e: Throwable) {
     logger.log(SEVERE, "Error", e)
@@ -112,12 +122,15 @@ fun main(vararg args: String) {
 private class WatchedBy(val user: String, val watched: Boolean)
 
 private fun Episode.toDisplayString(maxNameLen: Int, maxShowNameLen: Int, now: Instant): String {
-  val time = DATE_FORMATTER.format(lastViewed)
   val show = showName.padEnd(maxShowNameLen)
-  val episode = "S${seasonNumber.pad()}E${episodeNumber.pad()}".padEnd(7)
+  val episodeId = toEpisodeId()
   val episodeName = name.padEnd(maxNameLen)
-  val daysAgo = Duration.between(lastViewed, now).toDays()
-  return "$time: $show $episode - $episodeName  - Watched $daysAgo days ago"
+  val status = when (lastViewed) {
+    Instant.EPOCH -> ""
+    else -> "  - Watched ${Duration.between(lastViewed, now).toDays()} days ago"
+  }
+  return "$show $episodeId - $episodeName$status"
 }
 
+private fun Episode.toEpisodeId() = "S${seasonNumber.pad()}E${episodeNumber.pad()}".padEnd(7)
 private fun Int.pad(): String = toString().padStart(2, '0')
