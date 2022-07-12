51c92c182fbb474c7d3bbf44cd46f56cb70f88bd
Alon Albert <aalbert@google.com>
2022-07-12 09:13:18 -0700

    Build executable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 plugins {
   application
@@ -25,19 +27,11 @@ dependencies {
 }
 
 tasks {
-  val fatJar = register<Jar>("fatJar") {
-    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
-    archiveClassifier.set("standalone") // Naming the jar
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
-    val sourcesMain = sourceSets.main.get()
-    val contents = configurations.runtimeClasspath.get()
-      .map { if (it.isDirectory) it else zipTree(it) } +
-        sourcesMain.output
-    from(contents)
-  }
+  val fatJar = register<FatJar>("fatJar")
+  val executableJar = register<ExecutableJar>("executableJar", fatJar)
+
   build {
-    dependsOn(fatJar) // Trigger fat jar creation during build
+    dependsOn(executableJar) // Trigger fat jar creation during build
   }
 }
 
@@ -47,4 +41,50 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "1.8"
+}
+
+abstract class FatJar : Jar() {
+  init {
+    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+    archiveClassifier.set("standalone")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    super.manifest { attributes(mapOf("Main-Class" to project.application.mainClass.get())) }
+
+    val sourcesOutput = project.sourceSets.main.get().output
+    val content = project.configurations.runtimeClasspath.get().map { if (it.isDirectory) it else project.zipTree(it) } + sourcesOutput
+    super.from(content)
+  }
+}
+
+abstract class ExecutableJar @Inject constructor(jarProvider: TaskProvider<Jar>) : DefaultTask() {
+  private val jar = jarProvider.get()
+
+  init {
+    super.dependsOn(jar)
+  }
+
+  @TaskAction
+  fun build() {
+    val inputJarFile = jar.archiveFile.get().asFile
+    val outputFile = "${project.buildDir}/distributions/${project.name}"
+    FileOutputStream(outputFile).use {
+      it.write(
+        """
+        #!/bin/sh
+
+        MYSELF=`which "${'$'}0" 2>/dev/null`
+        [ ${'$'}? -gt 0 -a -f "${'$'}0" ] && MYSELF="./${'$'}0"
+        java=java
+        if test -n "${'$'}JAVA_HOME"; then
+            java="${'$'}JAVA_HOME/bin/java"
+        fi
+        java_args=-Xmx1g
+        exec "${'$'}java" ${'$'}java_args -jar ${'$'}MYSELF "${'$'}@"
+        exit 1 
+      """.trimIndent().toByteArray()
+      )
+      it.write(FileInputStream(inputJarFile).readAllBytes())
+    }
+    File(outputFile).setExecutable(true)
+  }
 }
