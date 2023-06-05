a02b9da214182b52eef46c92cbfceb4be9ffec32
Alon Albert <aalbert@google.com>
2023-06-05 16:21:03 -0700

    Serve json

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 group = "org.alonalbert"
 
 dependencies {
+  implementation(libs.gson)
+  implementation(libs.kotlinx.cli)
   implementation(libs.ktor.server.core)
   implementation(libs.ktor.server.netty)
+  implementation(libs.log4j.api)
+  implementation(libs.log4j.core)
+  implementation(libs.log4j.slf4j.impl)
 }
--- config-server/src/main/kotlin/com/aa/plexautodelete/configserver/ConfigServer.kt
+++ config-server/src/main/kotlin/com/aa/plexautodelete/configserver/ConfigServer.kt
@@ -6,13 +6,30 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import java.nio.file.Paths
+import kotlin.io.path.reader
+
+val DEFAULT_CONFIG_FILE = "${System.getProperty("user.home")}/.plex-auto-delete-config.json"
 
 fun main(args: Array<String>) {
-  embeddedServer(Netty, 8080) {
+  val parser = ArgParser("Plex Auto Delete Config Server")
+  val configFile by parser.option(ArgType.String, shortName = "c", description = "Config file").default(DEFAULT_CONFIG_FILE)
+  val port by parser.option(ArgType.Int, shortName = "p", description = "Port number").default(8080)
+  parser.parse(args)
+
+  embeddedServer(Netty, port) {
     routing {
       get("/") {
-        call.respondText("Hello, world!", ContentType.Text.Html)
+        try {
+          call.respondText(Paths.get(configFile).reader().readText(), ContentType.Application.Json)
+        } catch (e: Throwable) {
+          this@embeddedServer.log.error("Error loading file", e)
+        }
       }
     }
+
   }.start(wait = true)
 }
