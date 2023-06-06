2c05f2ff64dcdae5daf029a3485b897b1615556e
Alon Albert <aalbert@google.com>
2023-06-06 11:27:42 -0700

    Build server jar

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -14,3 +14,23 @@ dependencies {
   implementation(libs.log4j.core)
   implementation(libs.log4j.slf4j.impl)
 }
+
+
+tasks.withType<Jar> {
+  enabled = true
+  isZip64 = true
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  manifest {
+    attributes(mapOf("Main-Class" to "com/aa/plexautodelete/configserver/ConfigServerKt"))
+  }
+  archiveFileName.set("${project.name}.jar")
+  dependsOn(configurations.compileClasspath)
+  val source = sourceSets.main.get().output
+  val libs = configurations.runtimeClasspath.get().map {
+    when {
+      it.isDirectory -> it
+      else -> zipTree(it)
+    }
+  }
+  from(source + libs)
+}
